712e5596977e6ec5b17c22a53328f7d5060cb17e
awsajju <aws.devops.123aws@gmail.com>
2025-02-03 10:27:45 +0530

    special varaible

---
+++ arrays/spec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All the varaible: $@"
+echo "Number of varaibles: $#"
+echo "all the vaaible in single string: $*"
+echo "present work directory: $pwd"
+echo "Which is user is running: $USER"
+echo "process id of current script: $$"
+echo "process id of last command in background: $!"
+echo "print the current sprint name: $0"
+
