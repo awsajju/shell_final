34df31d2a207456bb0098784f2ec44733be9ff67
awsajju <aws.devops.123aws@gmail.com>
2025-02-03 11:19:22 +0530

    condi

--- condition.sh/02-cond.sh
+++ condition.sh/02-cond.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Emter the number"
+echo "Enter the number"
 
 read Num
 
 if [ $Num -gt 0 ]; then
     echo "given number positive"
-else
-    echo "given number is negative"
 elif [ $Num == 0 ]; then
     echo "given number is zero"
+else
+    echo "given number is negative"
 
 fi
