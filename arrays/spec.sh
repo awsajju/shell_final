0382115cf991ca67951aadfbd9c714d2967aa8f3
awsajju <aws.devops.123aws@gmail.com>
2025-02-03 10:29:59 +0530

    special varaible

--- arrays/spec.sh
+++ arrays/spec.sh
@@ -3,7 +3,7 @@
 echo "All the varaible: $@"
 echo "Number of varaibles: $#"
 echo "all the vaaible in single string: $*"
-echo "present work directory: $pwd"
+echo "present work directory: $PWD"
 echo "Which is user is running: $USER"
 echo "process id of current script: $$"
 echo "process id of last command in background: $!"
