c76c07cb25f2b49f34376c654d86d3da76f7bf04
awsajju <aws.devops.123aws@gmail.com>
2025-02-03 12:41:46 +0530

    installing package using function

--- function/main-fun.sh
+++ function/main-fun.sh
@@ -5,18 +5,19 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-    echo "you must have the sudo access to executre this"
+    echo -e $R "you must have the sudo access to executre this $N"
     exit 1
 fi
 
 validate (){
     if [ $1 -ne 0 ];then
-        echo "$2....failure"
+        echo -e  "$2....$R failure $N"
         exkit 1
     else
-        echo "$2....success"
+        echo -e "$2....$G success $N"
     fi
 }
 
@@ -25,5 +26,5 @@ if [ $? -ne 0 ]; then
     dnf install python -y
     validate $? "installing pyhton"
 else
-    echo "already installed"
+    echo -e "$Y already installed $N"
 fi
