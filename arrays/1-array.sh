dc227baa21e19d5d80a4c027a6d45816ff6e0353
awsajju <aws.devops.123aws@gmail.com>
2025-02-03 09:52:35 +0530

    arrays

--- arrays/1-array.sh
+++ arrays/1-array.sh
@@ -2,6 +2,6 @@
 
 movie=("sye","ready","RRR","pushpa" )
 
-echo "the first movie is ${movie[1]}"
-echo "all movies passed in the array ${movie[@]}"
-echo "number movies in array ${movie[#]}"
+echo "the first movie is" ${ movie[1] }
+echo "all movies passed in the array" ${ movie[@] }
+echo "number movies in array" ${ movie[#] }
