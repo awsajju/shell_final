2c1d57efbfcd073fe955d427a6dd9eaae884857b
awsajju <aws.devops.123aws@gmail.com>
2025-02-03 11:43:04 +0530

    installing packages

---
+++ installing/mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Enter the package"
+read package
+
+USERID = $(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "you must have the execute sudo access to execute this"
+    exit 1
+fi
+
+dnf list insalled mysql -y
+if [ $? -ne 0 ]; then
+
+    dnf install mysql -y
+    if [ $? -ne 0 ]; then
+        echo "installing mysql..... failure"
+        exit 1
+    else
+        echo "installing mysql..... success"
+    fi
+elif
+    echo "already installed"
+fi
+
+dnf list installed git -y
+if [ $? -ne 0 ]; then
+
+    dnf install git -y
+    if [ $? -ne 0 ]; then
+        echo "installing git .... failure"
+        exit 1
+    else
+        echo "installing git .... success"
+    fi
+elif
+    echo "already installed"
+
+fi
